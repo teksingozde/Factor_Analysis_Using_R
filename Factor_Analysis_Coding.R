f13e9b52b15ba9e55c111f70307194455463beb0
Guner Gozde Teksin Kilic <ggteksin@gmail.com>
2023-03-26 00:43:03 -0500

    Created R Script
    Explained coding for factor analysis.

---
+++ Factor_Analysis_Coding.R
@@ -0,0 +1,36 @@
+# Import Dataset
+library(readxl)
+Factor_Analysis_Survey_Dataset <- read_excel("Documents/GitHub/Factor_Analysis_Using_R/Factor Analysis Survey Dataset.xlsx", col_names = FALSE)
+                                             
+# Multivariate Normality Test                                            
+library(MVN)
+install.packages("mardiatest")
+result <- mardiaTest(Factor_Analysis_Survey_Dataset, qqplot=TRUE)
+result
+
+# Henze-Zirkler Multivariate Normality Test
+result1 <- mvn(data = Factor_Analysis_Survey_Dataset, mvnTest = "hz")
+result1
+
+# Summary Dataset
+summary(Factor_Analysis_Survey_Dataset)
+
+# Correlation Matrix
+correlation <- cor(Factor_Analysis_Survey_Dataset, method = "spearman")
+correlation
+
+# Bartlett Test
+install.packages("psych")
+library(psych)
+cortest.bartlett(correlation,  n=nrow(Factor_Analysis_Survey_Dataset))
+
+# KMO Test
+KMO(correlation)
+
+# Eigenvalues
+eigen(correlation)
+
+# Scree Plot
+install.packages("nFactors")
+library(nFactors)
+scree(correlation,hline=-1) #hline=-1 draw a horizontal line at -1
